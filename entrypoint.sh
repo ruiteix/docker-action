8b9d0f40700dca00682cfe4f5aa0970619d83d7e
Rui <ruiteix@yahoo.fr>
2020-06-23 19:36:31 +0200

    My first action is ready

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
+[[ $GITHUB_REF =~ "refs/heads/" ]] && _GITHUB_BRANCH=$(echo $GITHUB_REF | cut -d/ -f3) || :
+[[ $GITHUB_REF =~ "refs/tags/" ]] && _GITHUB_TAG=$(echo $GITHUB_REF | cut -d/ -f3) || :
+echo ::set-env name=CI_COMMIT_REF_NAME::${GITHUB_HEAD_REF:-${_GITHUB_BRANCH:-$_GITHUB_TAG}}
+echo ::set-env name=CI_COMMIT_BRANCH::${GITHUB_HEAD_REF:-$_GITHUB_BRANCH}
+echo ::set-env name=CI_COMMIT_TAG::${_GITHUB_TAG}
+echo ::set-env name=CI_COMMIT_SHA::${GITHUB_COMMIT_PR_SHA:-$GITHUB_SHA}
